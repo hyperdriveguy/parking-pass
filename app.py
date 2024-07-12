6134667f8e3e626e940ba451105e582f79a1e765
Carson Bush <hyperdriveguy@gmail.com>
2024-07-12 00:25:53 -0600

    futureproof get latest data

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, jsonify, Response
 import csv
-from pass_history import scrape_and_update_data, get_cached_plots_filenames
-import time
 import json
+import time
+from collections import defaultdict
+
+from flask import Flask, Response, jsonify, render_template
+
+from pass_history import get_cached_plots_filenames, scrape_and_update_data
 
 subdir_endpoint = '/parking-watch'
 
@@ -22,10 +25,20 @@ def read_data_from_csv():
     return data
 
 def get_latest_data(data):
-    if data:
-        latest_data = [data[-2], data[-1]]
-    else:
-        latest_data = []
+    if not data:
+        return []
+
+    # Group the data by pass type
+    data_by_type = defaultdict(list)
+    for row in data:
+        pass_type = row['Pass Type']
+        data_by_type[pass_type].append(row)
+
+    # Get the latest data for each pass type
+    latest_data = []
+    for pass_type, type_data in data_by_type.items():
+        latest_data.append(type_data[-1])
+
     return latest_data
 
 @app.route(subdir_endpoint + '/')
